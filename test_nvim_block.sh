c731fec7145b3f5b25606b363f3c8a5964c2bbed
zacmero <z4cmero@gmail.com>
2026-02-25 22:53:01 +0000

    refactor: remove obsolete test_nvim_block.sh script

--- test_nvim_block.sh
+++
@@ -1,9 +0,0 @@
-ARCH_TYPE="arm64"
-if [ "$ARCH_TYPE" = "x64" ]; then
-    NVIM_TAR="nvim-linux-x86_64.tar.gz"
-elif [ "$ARCH_TYPE" = "arm64" ]; then
-    NVIM_TAR="nvim-linux-arm64.tar.gz"
-else
-    NVIM_TAR=""
-fi
-echo $NVIM_TAR
